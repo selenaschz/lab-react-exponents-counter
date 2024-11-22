e8f8aab91c6592aa1269c2d39d723d1d040e5e81
Selena Sánchez <sele71098@gmail.com>
2024-11-22 23:57:01 +0100

    correct symbol in string to '*'

--- src/components/Exponent.jsx
+++ src/components/Exponent.jsx
@@ -1,8 +1,8 @@
 const Exponent = ({ num, exponent }) => {
     return (
         <div className="exponent-counter-container">
-        <p className="exponent-label">{num}<sup> {exponent} </sup></p>
-        <p className="exponent-result">{Array(exponent).fill(num).join(" + ")} = <span className="total"> {Math.pow(num, exponent)}</span></p>
+        <p className="exponent-label">n<sup> {exponent} </sup></p>
+        <p className="exponent-result">{Array(exponent).fill(num).join(" * ")} = <span className="total"> {Math.pow(num, exponent)}</span></p>
       </div>
     );
 }
